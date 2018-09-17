2f50b233029638af1b191b1c075ecc2ab7193cc5
Dragos Busuioc <dragosbusuioc94@gmail.com>
2018-09-17 15:35:49 +0000

    cookie

--- express-basics/server.js
+++ express-basics/server.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 app.set("view engine", "pug");
 
-app.get("/", (req, res)=> {
-    const colors = ["red", "green", "blue", "yellow"];
-    res.locals.prompt = "Who is burriend?";
-    res.locals.hint = "hello";
-    res.locals.colors = colors;
+app.get('/welcome', (req, res)=>{
+    console.log(req.cookies.username);
+    res.render("welcome", {user: req.cookies.username}); 
+});
+
+app.get("/hello", (req, res)=> {
     res.render("index"); 
+});
+
+app.post('/hello', (req, res)=>{
+    if(req.body.username) {
+        res.cookie('username', req.body.username);
+        res.redirect('/welcome');
+    }
+    res.redirect('/hello');
 });
 
 app.listen(process.env.PORT, process.env.IP, ()=> {
